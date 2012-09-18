bd5d10e629415314e733f2722541844a947d6545
David N. Robinson <dave@drob.co>
2012-09-18 00:13:17 -0400

    Added header file for the ppm function library

--- ppm.h
+++ ppm.h
@@ -1,205 +1,82 @@
 /********************************************************************
  * FILE: ppm.h
  * -----------
- *  PPM is an image format that is extremely easy to open and 
- *  manipulate using basic programming. This library seeks to make
- *  such image processing easier by abstracing out some data types
- *  and functions for use with opening and working with PPM image
- *  files.
+ *  This is a library that aims to make importing images in the PPM
+ *  and PGM formats easy to import and parse into appropriate data
+ *  structures. This library depends on the "imgStruct.h" header.
  *
  *  AUTHOR: David N. Robinson
- *  DATE: 09/16/2012
- *  VERSION: 0.1a
+ *  DATE: 09/17/2012
  *  LICENSE: GPLv3
- *******************************************************************/
+ */
 
 #ifndef UTILS_PPM
 #define UTILS_PPM
 
 /********************************************************************
- * TYPE: ColorPixel
- * -----------
- *  Definition of type pixel to store the color value for a single
- *  pixel. Each color value is stored as an in 0 < i < 255 for 3
- *  discrete color channels. By default the three channels will 
- *  represent a RGB value. For other color-spaces, such as HSV, there
- *  will be another pixel struct
+ * FUNCTION: importPPM(char* path, ColorImage image)
+ * -------------------------------------------------
+ *  This function imports a PPM image file and parses it into a 
+ *  ColorImage struct. Once parsed, the image will be able to be 
+ *  manipulated by other functions.
+ *  
+ *  Here is an example, inputImg is a ColorImage, path is a char*
+ *
+ *      importPPM(path, inputImg);
  */
-struct colorpixel;
-typedef struct colorpixel *ColorPixel;
+void importPPM(char* path, ColorImage image);
 
 /********************************************************************
- * TYPE: BinaryPixel
- * -----------
- *  Definition of type pixel to store the either a black or a white
- *  color value in a single pixel. 0 is black (unselected) and 0 is
- *  white (selected).
+ * FUNCTION: importPGM(char* path, ColorImage image)
+ * -------------------------------------------------
+ *  This function imports a PGM image file and parses it into a
+ *  BWImage struct. Once parsed, the image will be able to be 
+ *  manipulated by other functions.
+ *
+ *  Here is an example, inputImg is a ColorImage, path is a char*
+ *
+ *      importPGM(path, inputImg);
  */
-struct binarypixel;
-typedef struct binarypixel *BinaryPixel;
+void importPGM(char* path, BWImage);
 
 /********************************************************************
- * TYPE: BWPixel
- * -----------
- *  Definition of type pixel to store the grey value for a single 
- *  pixel. all grey values will be 0 < i < 255.
+ * FUCNTION: exportRGBPPM(char* path, ColorImage image)
+ * ----------------------------------------------------
+ *  This function exports a PPM image file from the data contained
+ *  within a ColorImage struct. Outputs to the path passed through
+ *  the parameter. NB: THIS FUNCTION ONLY WORKS WITH AN RGB COLOR
+ *  SPACE.
+ *
+ *  Here is an example, outputImg is a ColorImage and path is a char*
+ *
+ *      exportRGBPPM(path, outputImg);
  */
-struct bwpixel;
-typedef struct bwpixel *BWPixel;
+void exportRGBPPM(char* path, ColorImage image);
 
 /********************************************************************
- * TYPE: HSVPixel
- * -----------
- *  Definition of type pixel to store the color value of each pixel.
- *  Each color value is stored as an in 0 < i < 255 for 3 discrete
- *  color channels (hue, saturation, luminosity). Those seeking an
- *  RGB color-space should seek out the ColorPixel typedef.
+ * FUNCTION: exportBinaryPGM(char* path, BinaryImage image)
+ * --------------------------------------------------------
+ *  This function exports a PGM image file from the data contained
+ *  within a BinaryImage struct. Outputs to the path passed through
+ *  the parameter. This function only works with binary images.
+ *
+ *  Here is an example, outputImg is a BinaryImage, path is a char*
+ *
+ *      exportBinaryPGM(path, outputImg);
  */
-struct hsvpixel;
-typedef struct hsvpixel *HSVPixel;
+void exportBinaryPGM(char* path, BinaryImage image);
 
 /********************************************************************
- * TYPE: ColorImage
- * -----------
- *  Definition of a struct consisting of a 2D array of ColorPixels.
- *  The combination of all the pixels creates a full image. This 
- *  particular struct uses ColorPixels, but others will use different
- *  pixel types. Also, each ColorImage struct stores the height
- *  and width value of the full image for easy retreaval
+ * FUNCTION: exportBWPGM(char* path, BWImage image)
+ * ------------------------------------------------
+ *  This function exports a PGM image file from the data contained
+ *  within a BWImage struct. Outputs to the path passed through the
+ *  parameter. This function only works with the bw images
+ *
+ *  Here is an example, outputImg is a BWImage, path is a char*
+ *
+ *      exportBWPGM(path, outputImg);
  */
-struct colorimage;
-typedef struct colorimage *ColorImage;
+void exportBWPGM(char* path, BWImage image);
 
-/********************************************************************
- * TYPE: BinaryImage
- * -----------
- *  Definition of a struct consisting of a 2D array of BinaryPixels.
- *  The combination of all the pixels creates a full image. This 
- *  particular struct uses BinaryPixels, but others will use different
- *  pixel types. Also, each BinaryImage struct stores the height
- *  and width value of the full image for easy retreaval
- */
-struct binaryimage;
-typedef struct binaryimage *BinaryImage;
-
-
-/********************************************************************
- * TYPE: BWImage
- * -----------
- *  Definition of a struct consisting of a 2D array of BWPixels.
- *  The combination of all the pixels creates a full image. This 
- *  particular struct uses BWPixels, but others will use different
- *  pixel types. Also, each BWImage struct stores the height
- *  and width value of the full image for easy retreaval
- */
-struct bwimage;
-typedef struct bwimage *BWImage;
-
-/********************************************************************
- * TYPE: HSVImage
- * -----------
- *  Definition of a struct consisting of a 2D array of HSVPixels.
- *  The combination of all the pixels creates a full image. This 
- *  particular struct uses HSVPixels, but others will use different
- *  pixel types. Also, each HSVImage struct stores the height
- *  and width value of the full image for easy retreaval
- */
-struct hsvimage;
-typedef struct hsvimage *HSVImage;
-
-/********************************************************************
- * FUNCTION: createColorPixel(int R, int G, int B)
- * -----------------------------------------------
- *  Function to properly allocate memory for a new ColorPixel data 
- *  type and return the pixel to the calling function.
- *
- *  Here is an example usage, i[][] is a 2D array of ColorPixels:
- *
- *      i[0][0] = createColorPixel(255, 255, 255);
- */
-ColorPixel createColorPixel(int R, int G, int B);
-
-/********************************************************************
- * FUNCTION: deleteColorPixel(ColorPixel pixel)
- * --------------------------------------------
- *  Function to properly de-allocate the memory of a specific
- *  ColorPixel struct passed through the parameter
- *
- *  Here is an example usage, p is a ColorPixel:
- *
- *      deleteColorPixel(p);
- */
-void deleteColorPixel(ColorPixel pixel);
-
-/********************************************************************
- * FUNCTION: createBinaryPixel(int value)
- * --------------------------------------
- *  Function to properly allocate memory for a new BinaryPixel data
- *  type and return the pixel to the calling function.
- *
- *  Here is an example usage, i[][] is a 2D array of BinaryPixels
- *
- *      i[0][0] = createBinaryPixel(0);
- */
-BinaryPixel createBinaryPixel(int value);
-
-/********************************************************************
- * FUNCTION: deleteBinaryPixel(BinaryPixel pixel)
- * ----------------------------------------------
- *  Function to properly de-allocate memory of a specific 
- *  BinaryPixel struct passed through the parameter.
- *
- *  Here is an example usage, b is a binary pixel:
- *
- *      deleteBinaryPixel(b);
- */
-void deleteBinaryPixel(BinaryPixel pixel);
-
-/********************************************************************
- * FUNCTION: createBWPixel(int value)
- * ----------------------------------
- *  Function to properly allocate memory for a new BWPixel data type
- *  and return the pixel to the calling function.
- *
- *  Here is an example usage; i[][] is a 2D array of BWPixels
- *
- *      i[0][0] = createBWPixel(114);
- */
-BWPixel createBWPixel(int value);
-
-/********************************************************************
- * FUNCTION: deleteBWPixel(BWPixel pixel)
- * --------------------------------------
- *  This function properly de-allocates memory for a specific 
- *  BWPixel struct passed through the parameter.
- *
- *  Here is an example usage, b is a BWPixel:
- *
- *      deleteBWPixel(b);
- */
-void deleteBWPixel(BWPixel pixel);
-
-/********************************************************************
- * FUNCTION: createHSVPixel(int H, int S, int V)
- * ----------------------------------
- *  Function to properly allocate memory for a new HSVPixel data type
- *  and return the pixel to the calling function.
- *
- *  Here is an example usage; i[][] is a 2D array of HSVPixels
- *
- *      i[0][0] = createHSVPixel(114);
- */
-HSVPixel createHSVPixel(int H, int S, int V);
-
-/********************************************************************
- * FUNCTION: deleteHSVPixel(HSVPixel pixel)
- * --------------------------------------
- *  This function properly de-allocates memory for a specific 
- *  BWPixel struct passed through the parameter.
- *
- *  Here is an example usage, b is a BWPixel:
- *
- *      deleteBWPixel(b);
- */
-void deleteHSVPixel(HSVPixel pixel);
-
+#endif
